a9058865ec2045f5201b40ab7622512862f6cb58
octps <renmo9@gmail.com>
2014-09-19 13:16:03 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+    require("lib/root.php");
+?>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>cat</title>
+  <link href="css/base.css" rel="stylesheet">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+
+  <script type="text/javascript">
+    function initialize() { 
+        var latlng = new google.maps.LatLng(<?= $mapValue[0] ?>, <?= $mapValue[1] ?>); 
+        var myOptions = { 
+          zoom: 18, 
+          center: latlng, 
+          mapTypeId: google.maps.MapTypeId.SATELLITE
+        }; 
+        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+
+        var myLatlng = new google.maps.LatLng(<?= $mapValue[0] ?>, <?= $mapValue[1] ?>);
+        var icon = new google.maps.MarkerImage('images/sample.png',
+          new google.maps.Size(73,51),
+          new google.maps.Point(0,0),
+          new google.maps.Point(19,51)
+        );
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            icon: icon,
+            title:"this"
+        });   
+    }
+
+
+    // google.maps.event.addDomListener(window, 'load', function() {
+    //   var map = document.getElementById("gmap");
+    //   var options = {
+    //     zoom: 18,
+    //     center: new google.maps.LatLng(<?= $mapValue[0] ?>, <?= $mapValue[1] ?>),
+        
+    //     mapTypeId: google.maps.MapTypeId.SATELLITE
+    //   };
+    //   new google.maps.Map(map, options);
+    // });
+  </script>
+
+</head>
+<body onload="initialize()">
+
+<header>
+cat
+</header>
+<div class="body">
+    <form action="/" method="post" enctype="multipart/form-data">
+        <input name="title" type="text" value="">
+        <input name="image" type="file" value="">
+        <input type="submit" value="送信">
+    </form>
+    <div id="map_canvas" style="width : 500px; height : 500px;">
+    </div>
+</div>
+<footer class="container">
+    footer
+</footer>
+</body>
+</html>
---
+++ lib/root.php
@@ -0,0 +1,64 @@
+<?php
+  if(isset($_FILES['image']['error']) && is_int($_FILES['image']['error'])) {
+    try {
+      switch ($_FILES['image']['error']) {
+        case UPLOAD_ERR_OK: // OK
+          break;
+        case UPLOAD_ERR_NO_FILE:   // ファイル未選択
+          throw new RuntimeException('ファイルが選択されていません');
+        case UPLOAD_ERR_INI_SIZE:  // php.ini定義の最大サイズ超過
+        case UPLOAD_ERR_FORM_SIZE: // フォーム定義の最大サイズ超過
+          throw new RuntimeException('ファイルサイズが大きすぎます');
+        default:
+          throw new RuntimeException('その他のエラーが発生しました');
+      }
+
+      if (!in_array(
+        $type = @exif_imagetype($_FILES['image']['tmp_name']),
+        array(
+          IMAGETYPE_GIF,
+          IMAGETYPE_JPEG,
+          IMAGETYPE_PNG,
+        ),
+        true
+      )) {
+          throw new RuntimeException('画像形式が未対応です');
+      }
+
+      if (!move_uploaded_file(
+        $_FILES['image']['tmp_name'],
+        $path = sprintf('./images/%s%s',
+              sha1_file($_FILES['image']['tmp_name']),
+              image_type_to_extension($type)
+        )
+      )) {
+          throw new RuntimeException('ファイル保存時にエラーが発生しました');
+      }
+
+      chmod($path, 0644);
+      $msg = array('green', 'ファイルは正常にアップロードされました');
+      print_r($path);
+      $exif = exif_read_data($path);
+      $mapValue = getGPS ($exif);
+    } catch (RuntimeException $e) {
+      $msg = array('red', $e->getMessage());
+    }
+  }
+
+  function getGPS ($exif){
+    $GPSLatitude0 = intval(str_replace('/1', '', $exif['GPSLatitude'][0]));
+    $GPSLatitude1 = intval(str_replace('/1', "", $exif['GPSLatitude'][1])) / 60;
+    $GPSLatitude2 = $exif['GPSLatitude'][2] / 100 / 60 / 60;
+    $GPSLatitude = $GPSLatitude0 + $GPSLatitude1 + $GPSLatitude2;
+
+    $GPSLongitude0 = intval(str_replace('/1', '', $exif['GPSLongitude'][0]));
+    $GPSLongitude1 = intval(str_replace('/1', "", $exif['GPSLongitude'][1])) / 60;
+    $GPSLongitude2 = $exif['GPSLongitude'][2] / 100 / 60 / 60;
+    $GPSLongitude = $GPSLongitude0 + $GPSLongitude1 + $GPSLongitude2;
+    $GPS[0] = $GPSLatitude;
+    $GPS[1] = $GPSLongitude;
+
+    return $GPS;
+  }
+
+?>
